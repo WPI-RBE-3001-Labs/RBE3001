14ee4623bd728ae9c8ea89884d95730b81e4aa19
JeffTolbert <jptolbert@wpi.edu>
2017-01-24 15:13:37 -0500

    Added PWM timing for Lab 1

---
+++ RBE_3001/Lab1.c
@@ -0,0 +1,52 @@
+/*
+ * Lab1_ADC.c
+ *
+ *  Created on: Jan 24, 2017
+ *      Author: vargoal
+ */
+#include "Lab1.h"
+
+void setupADCTiming() {
+
+}
+
+void startADCOutput() {
+
+}
+
+void setupPWMTiming() {
+	//puts timer two in fast PWM mode
+	DDRD |= (1<<PD4); // set PWM pin as output
+	//PORTD = 0xFF;
+	TCCR1A = (1<<COM1B1)|(1<<WGM11)|(1<<WGM10);
+	TCCR1B = (1<<WGM12)|(1<<WGM13)|(1<<CS12)|(1<<CS10);
+	OCR1A = 200;//(0xFFFF)/100;
+	OCR1B = 100;//(0xFFFF)/200;
+}
+
+void setPWM(unsigned int newFreq,unsigned int dutyCycle) {
+	OCR1A = 18000/newFreq;
+	OCR1B = (OCR1A*dutyCycle)/100;
+}
+
+void startGlobalTiming() {
+
+}
+
+unsigned long getGlobalTime() {
+	return 1;
+}
+
+void setup() {
+	//setupADCTiming();
+	//startADCOutput();
+	setupPWMTiming();
+	setPWM(150,50);
+	//startGlobalTiming();
+	//setPWMFrew(140);
+}
+
+void loop() {
+	//TODO: Check for BTN press for ADC start
+	//TODO: check PWM freq BTNS to switch freq; Could do with interrupts but why
+}
---
+++ RBE_3001/Lab1.h
@@ -0,0 +1,25 @@
+/*
+ * Lab1_ADC.h
+ *
+ *  Created on: Jan 24, 2017
+ *      Author: vargoal
+ */
+
+#ifndef LAB1_H_
+#define LAB1_H_
+
+#include <RBELib/RBELib.h>
+
+void setupADCTiming();
+void startADCOutput();
+
+void setupPWMTiming();
+void setPWM(unsigned int newFreq,unsigned int dutyCycle);
+
+void startGlobalTiming();
+unsigned long getGlobalTime();
+
+void setup();
+void loop();
+
+#endif /* LAB1_H_ */
