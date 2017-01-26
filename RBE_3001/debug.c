07270075ba913a366354abbfe77e19c95e55e21b
JeffTolbert <jptolbert@wpi.edu>
2017-01-25 20:49:15 -0500

    Add printf function

---
+++ RBE_3001/debug.c
@@ -0,0 +1,20 @@
+/*
+ * debug.c
+ *
+ *  Created on: Jan 25, 2017
+ *      Author: vargoal
+ */
+
+#include "RBELib/RBELib.h"
+
+static FILE outputFile = FDEV_SETUP_STREAM(printfRBE, NULL, _FDEV_SETUP_WRITE);
+
+
+int printfRBE(char var, FILE *stream) {
+  putCharDebug(var);
+  return 0;
+}
+
+void initRBELib(){
+  stdout = &outputFile;
+}
