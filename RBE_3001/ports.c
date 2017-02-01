6596981a78f7bb48cf32b25806bd4fc92fedf103
JeffTolbert <jptolbert@wpi.edu>
2017-02-01 14:24:45 -0500

    Added port manipulation code.

--- RBE_3001/ports.c
+++ RBE_3001/ports.c
@@ -4,7 +4,9 @@
  *  Created on: Feb 1, 2017
  *      Author: vargoal
  */
-#include <RBELib/ports.h>
+#include <RBELib/RBELib.h>
+#include <RBELib/reg_structs.h>
+
 /**
  * @brief Sets the direction (Input/Output) of the specified pins.
  *
@@ -14,10 +16,64 @@
  * direction of.
  * @param ... Pins one after another
  *
- * @todo Create a way to set a port's pins to inputs or outputs.
  */
-void setPinsDir(char port, int dir, char numPins, ...) {
-
+void setPinsDir(char port, int dir, int numPins, ...) {
+	va_list pinPointers; //var args pointer list
+	va_start(pinPointers, numPins);
+	__8bitreg_t* portPointer; //Byte type defined in RBELib
+	//Note DDRAbits-DDRDbits defined in RBELib
+	switch(port)
+	{
+	case 'A':
+		portPointer = &DDRAbits;
+		break;
+	case 'B':
+		portPointer = &DDRBbits;
+		break;
+	case 'C':
+		portPointer = &DDRCbits;
+		break;
+	case 'D':
+		portPointer = &DDRDbits;
+		break;
+	default: //invalid input port so print error to serial
+		printf("Invalid port in setPinsDir");
+		break;
+	}
+	for(uint8_t i = 0;i<numPins;i++)
+	{
+		uint8_t pin = va_arg(pinPointers,int);
+		switch(pin)
+		{
+		case 0:
+			portPointer->_P0 = dir;
+			break;
+		case 1:
+			portPointer->_P1 = dir;
+			break;
+		case 2:
+			portPointer->_P2 = dir;
+			break;
+		case 3:
+			portPointer->_P3 = dir;
+			break;
+		case 4:
+			portPointer->_P4 = dir;
+			break;
+		case 5:
+			portPointer->_P5 = dir;
+			break;
+		case 6:
+			portPointer->_P6 = dir;
+			break;
+		case 7:
+			portPointer->_P7 = dir;
+			break;
+		default: //invalid pin number given
+			printf("Invalid pin in setPinsDir");
+			break;
+		}
+	}
 }
 
 /**
@@ -29,10 +85,66 @@ void setPinsDir(char port, int dir, char numPins, ...) {
  *
  * @return value The value of the specified pins on the port.
  *
- * @todo Create a way to read all given pins on a port.
  */
 unsigned char getPinsVal(char port, int numPins, ...) {
-	return 'A';
+	va_list pinPointers; //var args pointer list
+	va_start(pinPointers, numPins);
+	unsigned char result;
+	__8bitreg_t* portPointer; //Byte type defined in RBELib
+	//Note DDRAbits-DDRDbits defined in RBELib
+	switch(port)
+	{
+	case 'A':
+		portPointer = &PINA;
+		break;
+	case 'B':
+		portPointer = &PINB;
+		break;
+	case 'C':
+		portPointer = &PINC;
+		break;
+	case 'D':
+		portPointer = &PIND;
+		break;
+	default: //invalid input port so print error to serial
+		printf("Invalid port in getPinsVal");
+		break;
+	}
+	for(uint8_t i = 0;i<numPins;i++)
+	{
+		uint8_t pin = va_arg(pinPointers,int);
+		switch(pin)
+		{
+		case 0:
+			result|= (portPointer->_P0)<<0;
+			break;
+		case 1:
+			result|= (portPointer->_P1)<<1;
+			break;
+		case 2:
+			result|= (portPointer->_P2)<<2;
+			break;
+		case 3:
+			result|= (portPointer->_P3)<<3;
+			break;
+		case 4:
+			result|= (portPointer->_P4)<<4;
+			break;
+		case 5:
+			result|= (portPointer->_P5)<<5;
+			break;
+		case 6:
+			result|= (portPointer->_P6)<<6;
+			break;
+		case 7:
+			result|= (portPointer->_P7)<<7;
+			break;
+		default: //invalid pin number given
+			printf("Invalid pin in getPinsVal");
+			break;
+		}
+	}
+	return result;
 }
 
 /**
@@ -43,10 +155,64 @@ unsigned char getPinsVal(char port, int numPins, ...) {
  * @param val The value (high/low) to set the pin to.
  * @param ... The pins one after another.
  *
- * @todo Create a way to set all given pins on a port.
  */
 void setPinsVal(char port, int val, int numPins, ...) {
-
+	va_list pinPointers; //var args pointer list
+	va_start(pinPointers, numPins);
+	__8bitreg_t* portPointer; //Byte type defined in RBELib
+	//Note DDRAbits-DDRDbits defined in RBELib
+	switch(port)
+	{
+	case 'A':
+		portPointer = &PORTAbits;
+		break;
+	case 'B':
+		portPointer = &PORTBbits;
+		break;
+	case 'C':
+		portPointer = &PORTCbits;
+		break;
+	case 'D':
+		portPointer = &PORTDbits;
+		break;
+	default: //invalid input port so print error to serial
+		printf("Invalid port in setPinsVal");
+		break;
+	}
+	for(uint8_t i = 0;i<numPins;i++)
+	{
+		uint8_t pin = va_arg(pinPointers,int);
+		switch(pin)
+		{
+		case 0:
+			portPointer->_P0 = val;
+			break;
+		case 1:
+			portPointer->_P1 = val;
+			break;
+		case 2:
+			portPointer->_P2 = val;
+			break;
+		case 3:
+			portPointer->_P3 = val;
+			break;
+		case 4:
+			portPointer->_P4 = val;
+			break;
+		case 5:
+			portPointer->_P5 = val;
+			break;
+		case 6:
+			portPointer->_P6 = val;
+			break;
+		case 7:
+			portPointer->_P7 = val;
+			break;
+		default: //invalid pin number given
+			printf("Invalid pin in setPinsVal");
+			break;
+		}
+	}
 }
 
 
